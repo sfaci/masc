822aba0efd9149cdb014cf237b665fc0241175f7
Santiago Faci <santiago.faci@gmail.com>
2017-11-30 02:43:17 +0100

    Added Wordpress class

--- MascEntry.py
+++ MascEntry.py
@@ -1,9 +1,12 @@
-# Entry is a filesystem archive, file or directory
+import magic
+
+
+# A filesystem entry: file or directory
 class MascEntry:
 
     # Apparently 'st_creator' and 'st_type' are only available in some Unix (like Unix)
-    # so that I have created another '__init__' method
-    def __init__(self, name, full_path, size, mode, atime, mtime, ctime, btime, st_type="", creator=""):
+    # so, by now, I put 'file' or 'dir' manually checking the type of the entry
+    def __init__(self, name, full_path, size, mode, atime, mtime, ctime, st_type, creator=""):
         self.name = name
         self.full_path = full_path
         self.size = size
@@ -11,16 +14,40 @@ class MascEntry:
         self.atime = atime
         self.mtime = mtime
         self.ctime = ctime
-        self.btime = btime
         self.st_type = st_type
         self.creator = creator
 
-    def __str__(self):
-        return self.path
 
+    # Return the content of a plain text file
     def get_content(self):
-        if self.st_type == "file":
+        if self.is_plain_text():
             file = open(self.full_path)
             content = file.read()
             file.close()
             return content
+
+
+    # Check if the entry is a file
+    def is_file(self):
+        if self.st_type == "file":
+            return True
+        else:
+            return False
+
+
+    # Check if the entry is a plain text file
+    def is_plain_text(self):
+        if not self.is_file():
+            return False
+
+
+        mg = magic.Magic(mime=True)
+        mimetype = mg.from_file(self.full_path)
+        if mimetype.startswith("text"):
+            return True
+        else:
+            return False
+
+
+    def __str__(self):
+        return self.full_path
--- masc.py
+++ masc.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+
 import os
 import magic
 from MascEntry import MascEntry
+from Wordpress import Wordpress
 
 TEST_DIR = "test/"
 WORDPRESS_DIR = TEST_DIR + "wordpress/"
 CWD = os.getcwd() + "/"
 
 entry_list = []
+
 
 # Scan a full dir recursively and store 'MascEntry' objects in a list
 def store_dir_content(dir_path):
@@ -17,8 +21,9 @@ def store_dir_content(dir_path):
             st_type = "dir"
         else:
             st_type = "file"
+
         masc_entry = MascEntry(entry.name, CWD + entry.path, entry_stat.st_size, entry_stat.st_mode, entry_stat.st_atime,
-                               entry_stat.st_mtime, entry_stat.st_ctime, entry_stat.st_birthtime, st_type)
+                               entry_stat.st_mtime, entry_stat.st_ctime, st_type)
         entry_list.append(masc_entry)
 
         if entry.is_dir():
@@ -26,7 +31,14 @@ def store_dir_content(dir_path):
 
 
 # Only to test store_dir_content function
-store_dir_content(WORDPRESS_DIR)
-for me in entry_list:
+#store_dir_content(WORDPRESS_DIR)
+#for me in entry_list:
+#    print(me.full_path)
+#    print (me.is_plain_text())
+
+wordpress = Wordpress(WORDPRESS_DIR)
+wordpress.list()
+for me in wordpress.entry_list:
     print(me.full_path)
-    print(magic.from_file(me.full_path))
+
+print(wordpress.files_count())
