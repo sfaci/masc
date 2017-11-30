03d1300485f7c9ab696a9a44e59776c9b45b6b0d
Santiago Faci <santiago.faci@gmail.com>
2017-11-30 04:09:52 +0100

    Added some hints to develop new features later

---
+++ Utils.py
@@ -0,0 +1,13 @@
+# Path where clean installations are stored to check with working installations
+TEST_PATH = "test/"
+
+# Class with some static methods to perform generic operations
+class Utils:
+
+    # TODO compare a working installation with a clean one
+    @staticmethod
+    def compare_clean_installation(cms_type, path):
+        # TODO check for the working version of the 'cms_type' installation
+        # and download from the Internet a clean one to compare
+
+        return None
