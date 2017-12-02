40f4fee5bfebf5c6c2f063d22bdc266a6f125d29
Santiago Faci <santiago.faci@gmail.com>
2017-12-02 03:14:02 +0100

    Early support for Drupal

--- Drupal.py
+++ Drupal.py
@@ -1,5 +1,8 @@
 from CMS import CMS
-from PrintUtils import print_debug
+import urllib
+import os
+from Constants import CACHE_DIR
+
 
 # This class represents a Drupal installation
 class Drupal(CMS):
@@ -7,15 +10,36 @@ class Drupal(CMS):
     def __init__(self, path, name):
         super().__init__(path, name)
 
+        if not os.path.isfile(os.path.join(path, "core/lib/Drupal.php")):
+            raise Exception("Fatal Error. This is not a Drupal installation.")
+
 
     def compare_with_clean_installation(self):
         pass
 
     def get_version(self):
-        pass
+        version_line = ""
+
+        with open(os.path.join(self.path, "core/lib/Drupal.php")) as file:
+            for line in file:
+                if "const VERSION =" in line:
+                    version_line = line.lstrip()
+                    break
+
+        slices = version_line.split("'")
+        return slices[1]
 
     def download_clean_installation(self):
-        pass
+
+        url = "https://ftp.drupal.org/files/projects/drupal-" + self.version + ".zip"
+        zip_file = CACHE_DIR + self.type + "-" + self.version + ".zip"
+
+        urllib.request.urlretrieve(url, zip_file)
+
+        if not os.path.isfile(zip_file):
+            return False
+
+        return True
 
     def search_suspect_files(self):
         results = []
@@ -29,7 +53,5 @@ class Drupal(CMS):
         return results
 
 
-    def search_malware_signatures(self):
-        results = []
-
-        return results
+    def cleanup_site(self):
+        pass
