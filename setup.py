8dc9c7bbca027d9a69eae15fef5796db4d11dca3
Santiago Faci <santiago.faci@gmail.com>
2017-12-02 14:23:06 +0100

    Files to package masc

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='masc',
+    version='0.1',
+    packages=['',],
+    include_package_data=True,
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    description='A malware web scanner',
+    author='Santiago Faci',
+    author_email='santiago.faci@gmail.com',
+    url='http://github.com/sfaci/masc',
+    download_url='http://github.com/sfaci/masc/',
+    keywords=['malware', 'scanner', 'security'],
+    long_description=open('README.md').read(),
+)
