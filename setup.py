b3e1ba53b5db2ee647f8465e85d8d8e08bd94101
Santiago Faci <santiago.faci@gmail.com>
2017-12-05 00:49:19 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pypandoc.pandoc_download import download_pandoc
 import pypandoc
 
 # generate README.txt from README.md
-download_pandoc()
+# download_pandoc()     # it only works under Mac OS X. With Debian, it crashes
 readme = pypandoc.convert_file('README.md', 'rst')
 file_readme = open('README.txt', 'w+')
 file_readme.write(readme)
@@ -19,7 +19,7 @@ setup(
     author='Santiago Faci',
     author_email='santiago.faci@gmail.com',
     url='http://github.com/sfaci/masc',
-    download_url='https://github.com/sfaci/masc/releases/download/masc-0.1/masc-0.1.tar.gz',
+    download_url='https://github.com/sfaci/masc/releases/download/masc-0.2.2/masc-0.2.2.tar.gz',
     keywords='malware scanner security',
     classifiers=['Development Status :: 3 - Alpha'],
     requires=['watchdog', 'yara', 'magic', 'termcolor', 'pypandoc']
